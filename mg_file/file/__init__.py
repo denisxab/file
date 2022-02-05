b0c66f56edb921ee02512dd2960e6d7ce739b369
Ваше Имя <you@example.com>
2022-02-05 22:21:31 +0300

    +

--- mg_file/file/__init__.py
+++ mg_file/file/__init__.py
@@ -1,3 +1,5 @@
+from .base_file import BaseFile, ConcatData, sha256sum
+from .base_folder import BaseFolder, Folder, DiffDir
 from .csv_file import CsvFile
 from .env_file import EnvFile
 from .json_file import JsonFile
